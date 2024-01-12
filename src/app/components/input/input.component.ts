3293198f5f383a628bb1a2c76d8d861958267daf
Maycon Padilha <maycon.jordan5000@gmail.com>
2024-01-12 17:59:05 -0300

    adicionando e editando item em angular

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Item } from 'src/app/interfaces/iItem';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
+})
+export class InputComponent implements OnInit, OnChanges {
+  @Input() itemQueVaiSerEditado!: Item;
+  editando = false;
+  textoBtn = 'SalvarItem'
+
+  valorItem!: string;
+  constructor(private listaService: ListaDeCompraService) {}
+
+  ngOnInit(): void {}
+
+  adicionarItem() {
+    console.log(this.valorItem);
+    this.listaService.adicionarItemNaLista(this.valorItem);
+    this.limparCampo();
+  }
+
+  limparCampo(){
+    this.valorItem = '';
+  }
+
+  editarItem(){
+    this.listaService.editarItemDaLista(this.itemQueVaiSerEditado, this.valorItem);
+    this.limparCampo();
+    this.editando = false;
+    this.textoBtn = 'Salvar item';
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(!changes['itemQueVaiSerEditado'].firstChange){
+      this.editando = true;
+      this.textoBtn = 'Editar Item';
+      this.valorItem = this.itemQueVaiSerEditado?.nome
+    }
+  }
+}
